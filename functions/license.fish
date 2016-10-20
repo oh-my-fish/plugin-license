1306dc4e67deddb0df28652d7787e1b71bec74ba
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-10-20 10:49:57 -0200

    Add default behavior

--- functions/license.fish
+++ functions/license.fish
@@ -1,5 +1,15 @@
 function license
-  set -l license $argv[1]
-  set -l body (curl --silent --header 'Accept: application/vnd.github.drax-preview+json' https://api.github.com/licenses/$license | jq .'body')
-  echo -e $body | sed -e 's/^"//' -e 's/"$//'
+  set -l base_url https://api.github.com/licenses
+  set -l headers 'Accept: application/vnd.github.drax-preview+json'
+
+  if test $argv[1]
+    set -l license $argv[1]
+    set -l res (curl --silent --header $headers $base_url/$license | jq .'body')
+    echo -e $res | sed -e 's/^"//' -e 's/"$//'
+  else
+    set -l res (curl --silent --header $headers $base_url)
+    echo "Available Licenses: "
+    echo
+    echo "$res" | jq .[].'key' | sed -e 's/^"//' -e 's/"$//'
+  end
 end
